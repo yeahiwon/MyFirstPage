ed947e02ac2e5b3dbe656b9ca929ef6d8af38334
yeahiwon <89372405+yeahiwon@users.noreply.github.com>
2021-10-26 15:57:00 +0900

    change_opacity_num

--- sketch.js
+++ sketch.js
@@ -1,13 +1,13 @@
 let cnv, d, g;
 let raindrop = [];
 
-  
+
 
 function setup() {
   cnv = createCanvas(400, 400);
   cnv.mouseWheel(changeSize);
   d = 0;
-  
+
   for(let i = 0; i < 1000; i++) {
     raindrop[i] = new Raindrop(random(0, 400), random(0, -1000), random(0, 10));
   }
@@ -15,9 +15,9 @@ function setup() {
 
 function draw() {
   background(220);
-  
 
-  
+
+
   for(let i=0; i < raindrop.length; i++) {
     let gravity = createVector(0, 0.01 * raindrop[i].mass);
     let wind = createVector(d, 0);
@@ -31,7 +31,7 @@ function draw() {
 }
 
 class Raindrop {
-  
+
   constructor(x, y, m) {
   this.pos = createVector(x, y);
   this.vel = createVector(0, 0);
@@ -40,22 +40,22 @@ class Raindrop {
   this.r = 0;
   this.c = color(18,105,random(100,255));
   this.mass = m;
-  this.opacity = 200;
+  this.opacity = 100;
   }
-  
+
   update() {
     this.vel.add(this.acc);
     this.pos.add(this.vel);
     this.acc.mult(0,0);
   }
-  
+
   show() {
     noStroke();
     fill(this.c);
     ellipse(this.pos.x, this.pos.y, 5, this.length);
 
   }
-  
+
   applyForce(force) {
     let f = force.copy();
     f.div(this.mass);
@@ -73,8 +73,8 @@ class Raindrop {
 }
 function changeSize(event) {
   if (event.deltaY > 0) {
-    d = d + 0.001;
+    d = d + 0.0001;
   } else {
-    d = d - 0.001;
+    d = d - 0.0001;
   }
 }
